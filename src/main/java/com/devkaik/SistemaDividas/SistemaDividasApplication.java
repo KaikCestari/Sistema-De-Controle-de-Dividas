7b60bb3aaf06f1fe144b651d1c1f4571c334539d
Your Name <you@example.com>
2025-08-09 22:42:57 -0300

    Inicio do projeto

---
+++ src/main/java/com/devkaik/SistemaDividas/SistemaDividasApplication.java
@@ -0,0 +1,13 @@
+package com.devkaik.SistemaDividas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDividasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDividasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkaik/SistemaDividas/SistemaDividasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkaik.SistemaDividas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDividasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
